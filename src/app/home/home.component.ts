dbb6013716a2890c51b5024d19c18a84fe7a06c1
Amir YAZIDI <amir.yazidi1@esprit.tn>
2026-04-16 19:19:31 +0100

    model

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../models/Product';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ export class HomeComponent {
   priceMax!:number
   color:string = "red"
   title:string = "Welcome to ProjectSoir";
-  products:any = [
+  products:Product[] = [
     {id:1,name: "Product 1", description: "Description of Product 1", price: 100, nbrLike:0, quantity:5},
     {id:2,name: "t-shirt 2", description: "Description of Product 2", price: 150, nbrLike:0, quantity:5 },
     {id:3,name: "iphone 3", description: "Description of Product 3", price: 200,  nbrLike:0, quantity:5}
@@ -22,8 +23,8 @@ export class HomeComponent {
   increment(i:number){
    this.products[i].nbrLike++
   }
-  buy(product:any){
-    
+  buy(product:Product){
+
     product.quantity--
   }
 }
