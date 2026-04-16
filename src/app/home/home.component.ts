6cf9cbf7133af6d47efddf880a36391f1fb307ec
Amir YAZIDI <amir.yazidi1@esprit.tn>
2026-04-16 18:57:58 +0100

    databinding

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  priceMax!:number
+  color:string = "red"
+  title:string = "Welcome to ProjectSoir";
+  products:any = [
+    {id:1,name: "Product 1", description: "Description of Product 1", price: 100, nbrLike:0, quantity:5},
+    {id:2,name: "t-shirt 2", description: "Description of Product 2", price: 150, nbrLike:0, quantity:5 },
+    {id:3,name: "iphone 3", description: "Description of Product 3", price: 200,  nbrLike:0, quantity:5}
+  ]
+
+  save(){
+    alert("hiii")
+  }
+  increment(i:number){
+   this.products[i].nbrLike++
+  }
+  buy(product:any){
+    
+    product.quantity--
+  }
+}
